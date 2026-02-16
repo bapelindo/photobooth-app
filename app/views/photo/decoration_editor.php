f152e6ff0e3924de9af0bc86cc1a6dab1efb9c09
Ahnri <bapelhacker@gmail.com>
2026-02-16 09:46:07 +0700

    decoration done

--- app/views/photo/decoration_editor.php
+++ app/views/photo/decoration_editor.php
@@ -635,10 +635,13 @@
 
         .tool-btn.danger {
             background: var(--secondary-color);
+            border-color: var(--secondary-color);
+            color: white;
         }
 
         .tool-btn.danger:hover {
             background: #ff4f6d;
+            border-color: #ff4f6d;
         }
 
         .layer-list {
@@ -1058,14 +1061,14 @@
                         <path
                             d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm2 4v-2H3c0 1.1.9 2 2 2zM3 9h2V7H3v2zm12 12h2v-2h-2v2zm4-18H9c-1.11 0-2 .9-2 2v10c0 1.1.89 2 2 2h10c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 12H9V5h10v10zm-8 6h2v-2h-2v2zm-4 0h2v-2H7v2z" />
                     </svg>
-                    <span>Ke Depan</span>
+                    <span>Depan</span>
                 </button>
                 <button class="tool-btn" onclick="sendToBack()">
                     <svg class="tool-icon" viewBox="0 0 24 24">
                         <path
                             d="M9 7H7v2h2V7zm0 4H7v2h2v-2zm0-8c-1.11 0-2 .9-2 2h2V3zm4 12h-2v2h2v-2zm6-12v2h2c0-1.1-.9-2-2-2zm-6 0h-2v2h2V3zM9 17v-2H7c0 1.1.89 2 2 2h2zm10-4h2v-2h-2v2zm0-4h2V7h-2v2zm0 8c1.1 0 2-.9 2-2h-2v2zM5 7H3v12c0 1.1.89 2 2 2h12v-2H5V7zm10-2h2V3h-2v2zm0 12h2v-2h-2v2z" />
                     </svg>
-                    <span>Ke Belakang</span>
+                    <span>Belakang</span>
                 </button>
                 <button class="tool-btn danger" onclick="deleteSelected()">
                     <svg class="tool-icon" viewBox="0 0 24 24">
